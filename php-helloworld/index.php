4d6824beec9d47d2405e0eec1e188d2c26b0409f
Student User <student@workstation.lab.example.com>
2020-11-24 06:46:13 -0500

    Changed index page contents.

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "/DO180-apps/php-helloworld. to jest moja zmiana . Dobra zmiana "\n";
+print "/DO180-apps/php-helloworld. to jest moja zmiana . Dobra zmiana \n";
 ?>
